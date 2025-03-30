5b10db578377ba88217b5a3deb5f7f11e56f1df9
Brandon <brandonnilly@gmail.com>
2025-03-30 11:11:41 -0600

    Add Type To ScrapingBytes Facade

--- app/Facades/ScrapingBytes.php
+++ app/Facades/ScrapingBytes.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Facade;
 
 class ScrapingBytes extends Facade
 {
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'scrapingbytes';
     }
